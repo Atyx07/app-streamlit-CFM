0d3f68fdb3e3a2bbfc81a577b40a07761b73bead
Antoine <antoine.robette@gmail.com>
2025-08-19 10:46:33 +0200

    Update menu.py

--- test/menu.py
+++ test/menu.py
@@ -1,22 +1,27 @@
 import streamlit as st
 
-st.set_page_config(page_title="Menu principal")
-st.title("Menu principal")
-st.write("Sélectionnez une app à lancer :")
+st.set_page_config(page_title="CFM data Menu")
+st.title("CFM data Menu")
+st.write("Select an app to launch :")
 
 # Crée deux colonnes pour organiser les apps
-col1, col2 = st.columns(2)
-
+col1, col2, col3 = st.columns(3)
 with col1:
-    st.subheader("App 1")
-    st.write("Description détaillée de l'App 1 : cette app permet de faire X, Y et Z.")
-    if st.button("Ouvrir App 1"):
+    st.subheader("App 1 (Gregor)")
+    st.write("This app processes unique Raspi files and returns the processed files")
+    if st.button("Open App 1"):
+        js = "window.open('https://cfmdataprocessingst-dupsgjztrwwvcedwrivyeb.streamlit.app/')"
+        st.components.v1.html(f"<script>{js}</script>")
+with col2:
+    st.subheader("App 2")
+    st.write("This app processes multiple Raspi files and returns a Zip with the processed files")
+    if st.button("Open App 2"):
         js = "window.open('https://app-app-cfm-hnm3xzambasqhe8wmustbo.streamlit.app/')"
         st.components.v1.html(f"<script>{js}</script>")
 
-with col2:
-    st.subheader("App 2")
-    st.write("Description détaillée de l'App 2 : cette app permet de gérer A, B et C.")
-    if st.button("Ouvrir App 2"):
+with col3:
+    st.subheader("App 3")
+    st.write("This app processes multiple Raspi files and returns a Zip with the processed files and a recap Excel file containing mean and max CO2, mean Vdot and mean dp1")
+    if st.button("Open App 3"):
         js = "window.open('https://app2-app-cfm-jujxzt5sesafcn38bzubtr.streamlit.app/')"
         st.components.v1.html(f"<script>{js}</script>")
