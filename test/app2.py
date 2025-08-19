70387d86bea8a21691aea6ef4fd494f530c95647
Antoine <antoine.robette@gmail.com>
2025-08-19 10:19:33 +0200

    Update app2.py

--- test/app2.py
+++ test/app2.py
@@ -203,18 +203,6 @@ if csv_files_raspi:
 # =========================
 
 # 1) Zip fichiers
-if raspi_only_files:
-    zip_buffer_raspi = BytesIO()
-    with zipfile.ZipFile(zip_buffer_raspi, "w") as zf:
-        for fname, fdata in raspi_only_files:
-            zf.writestr(fname, fdata)
-    st.download_button(
-        label="⬇ Download all the results (RasPi seul)",
-        data=zip_buffer_raspi.getvalue(),
-        file_name="results_raspi_only.zip",
-        mime="application/zip"
-    )
-
 if extended_files:
     zip_buffer = BytesIO()
     with zipfile.ZipFile(zip_buffer, "w") as zip_file:
